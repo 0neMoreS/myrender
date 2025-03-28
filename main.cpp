5c7a0beff61219bba0159f09e8acbc9fa14e59b4
李博轩 <211250189@smail.nju.edu.cn>
2025-03-28 22:27:36 +0800

    strange projection

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-#include "tgaimage.cpp"
+#include <vector>
+#include <cmath>
+#include "tgaimage.cpp" //tga画图库
+#include "model.cpp"    //模型类，主要实现模型的读取
+#include "geometry.cpp" //几何库，主要定义了Vec2和Vec3类型
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+// 定义宽度高度
+const int width = 800;
+const int height = 800;
+const int l = -1, b = -1, n = 1, r = 1, t = 1, f = -1;
+
+// 4d-->3d
+// 除以最后一个分量。（当最后一个分量为0，表示向量）
+// 不为0，表示坐标
+Vec3f m2v(Matrix m)
+{
+    return Vec3f(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
+}
+
+// 3d-->4d
+// 添加一个1表示坐标
+Matrix v2m(Vec3f v)
+{
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -27,10 +56,35 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 
 int main(int argc, char **argv)
 {
-    TGAImage image(100, 100, TGAImage::RGB);
-    line(13, 20, 80, 40, image, white); // 线段A
-    line(20, 13, 40, 80, image, red);   // 线段B
-    line(80, 40, 13, 20, image, red);   // 线段C
+    TGAImage image(width, height, TGAImage::RGB);
+    model = new Model("./obj/african_head.obj ");
+    for (int i = 0; i < model->nfaces(); i++)
+    {
+        std::vector<int> face = model->face(i);
+        for (int k = 0; k < 3; k++)
+        {
+            Vec3f v0 = model->vert(face[k]);
+            Vec3f v1 = model->vert(face[(k + 1) % 3]);
+
+            Matrix project(4, 4);
+            project[0][0] = 2 * n / (r - l);
+            project[0][2] = (l + r) / (l - r);
+            project[1][1] = 2 * n / (t - b);
+            project[1][2] = (b + t) / (b - t);
+            project[2][2] = (f + n) / (n - f);
+            project[2][3] = 2 * f * n / (f - n);
+            project[3][2] = 1;
+
+            Vec3f v0_pro = m2v(project * v2m(v0));
+            Vec3f v1_pro = m2v(project * v2m(v1));
+
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(v0_pro[0], v0_pro[1], v1_pro[0], v1_pro[1], image, white);
+        }
+    }
     image.flip_vertically();
     image.write_tga_file("output.tga");
     return 0;
--- model.h
+++ model.h
@@ -4,17 +4,19 @@
 #include <vector>
 #include "geometry.h"
 
-class Model {
+class Model
+{
 private:
-	std::vector<Vec3f> verts_;
-	std::vector<std::vector<int> > faces_;
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<int>> faces_;
+
 public:
-	Model(const char *filename);
-	~Model();
-	int nverts();
-	int nfaces();
-	Vec3f vert(int i);
-	std::vector<int> face(int idx);
+    Model(const char *filename);
+    ~Model();
+    int nverts();
+    int nfaces();
+    Vec3f vert(int i);
+    std::vector<int> face(int idx);
 };
 
 #endif //__MODEL_H__
