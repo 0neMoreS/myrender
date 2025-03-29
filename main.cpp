c4ff880ccc07dda95df55b3ba230a98a62971620
李博轩 <211250189@smail.nju.edu.cn>
2025-03-29 21:52:53 +0800

    simple zbuffer, need fix with int

--- main.cpp
+++ main.cpp
@@ -1,84 +1,23 @@
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include "tgaimage.cpp" //tga画图库
-#include "model.cpp"    //模型类，主要实现模型的读取
-#include "geometry.cpp" //几何库，主要定义了Vec2和Vec3类型
 #include "utils.h"
-
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-Model *model = NULL;
-// 定义宽度高度
-const int width = 800;
-const int height = 800;
-const float K_d = 0.8;
-const int l = -1, b = -1, n = 1, r = 1, t = 1, f = -1;
-Ray light(Vec3f{0.0, 0.0, 10.0}, Vec3f{0.0, 0.0, 0.0});
-
-void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
-{
-    bool lean = false;
-    if (std::abs((float)(y1 - y0) / (float)(x1 - x0)) > 1)
-    {
-        lean = true;
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-    }
-    if (x0 > x1)
-    {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
-    for (int x = x0; x < x1; x++)
-    {
-        float t = (float)(x - x0) / (float)(x1 - x0);
-        int y = y0 * (1 - t) + y1 * t;
-        image.set(lean ? y : x, lean ? x : y, color);
-    }
-}
-
-void draw_triangle(ScreenTriangle &t, TGAImage &image, TGAColor color)
-{
-    for (int y = t.ts[0].y; y < t.ts[2].y; y++)
-    {
-        int x0 = (float)(y - t.ts[2].y) / (float)(t.ts[0].y - t.ts[2].y) * (t.ts[0].x - t.ts[2].x) + t.ts[2].x;
-        int x1 = 0;
-        if (y < t.ts[1].y)
-        {
-            x1 = (float)(y - t.ts[1].y) / (float)(t.ts[0].y - t.ts[1].y) * (t.ts[0].x - t.ts[1].x) + t.ts[1].x;
-        }
-        else
-        {
-            x1 = (float)(y - t.ts[1].y) / (float)(t.ts[2].y - t.ts[1].y) * (t.ts[2].x - t.ts[1].x) + t.ts[1].x;
-        }
-        if (x0 > x1)
-        {
-            std::swap(x0, x1);
-        }
-        for (int x = x0; x < x1; x++)
-        {
-            image.set(x, y, color);
-        }
-    }
-}
 
 int main(int argc, char **argv)
 {
     TGAImage image(width, height, TGAImage::RGB);
     model = new Model("./obj/man.obj ");
+    init_zbuffer();
+
     for (int i = 0; i < model->nfaces(); i++)
     {
         std::vector<Vec3f> verts{3};
         std::vector<Vec3f> normals{3};
-        std::vector<Vec2i> screen_tris{3};
+        std::vector<Vec3f> screen_tris{3};
 
         for (int j = 0; j < 3; j++)
         {
             verts[j] = model->vert(i, j);
             normals[j] = model->normal(i, j);
-            int x = (verts[j].x + 1.0) * width / 2.0, y = (verts[j].y + 1.0) * height / 2.0;
-            screen_tris[j] = {x, y};
+            float x = (verts[j].x + 1.f) * width / 2.f, y = (verts[j].y + 1.f) * height / 2.f;
+            screen_tris[j] = {x, y, verts[j].z};
         }
 
         // for (int j = 0; j < 3; j++)
--- utils.h
+++ utils.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <vector>
 #include <cmath>
 #include <algorithm>
@@ -6,7 +5,7 @@
 #include "model.cpp"    //模型类，主要实现模型的读取
 #include "geometry.cpp" //几何库，主要定义了Vec2和Vec3类型
 
-bool tri_compare(const Vec2i &a, const Vec2i &b)
+bool tri_compare(const Vec3i &a, const Vec3i &b)
 {
     if (a.y != b.y)
         return a.y < b.y;
@@ -15,8 +14,8 @@ bool tri_compare(const Vec2i &a, const Vec2i &b)
 
 struct ScreenTriangle
 {
-    std::vector<Vec2i> ts;
-    ScreenTriangle(std::vector<Vec2i> &_ts) : ts(_ts)
+    std::vector<Vec3f> ts;
+    ScreenTriangle(std::vector<Vec3f> &_ts) : ts(_ts)
     {
         std::sort(ts.begin(), ts.end(), tri_compare);
     }
@@ -27,3 +26,94 @@ struct Ray
     Vec3f o, d;
     Ray(Vec3f _o, Vec3f _d) : o(_o), d(_d) {}
 };
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+// 定义宽度高度
+const int width = 800;
+const int height = 800;
+const float K_d = 0.8;
+const int l = -1, b = -1, n = 1, r = 1, t = 1, f = -1;
+Ray light(Vec3f{0.0, 0.0, 10.0}, Vec3f{0.0, 0.0, 0.0});
+float zbuffer[width][height];
+
+void init_zbuffer()
+{
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            zbuffer[i][j] = std::numeric_limits<float>::min();
+        }
+    }
+}
+
+Vec3f barycentric(ScreenTriangle &t, Vec2f P)
+{
+    Vec3f AB = t.ts[1] - t.ts[0];
+    Vec3f AC = t.ts[2] - t.ts[0];
+    Vec3f PA = t.ts[0] - Vec3f{P.x, P.y, 0.f};
+
+    Vec3f uv = cross(Vec3f{AB.x, AC.x, PA.x}, Vec3f{AB.y, AC.y, PA.y});
+
+    if (std::abs(uv[2]) > 1e-2)
+    {
+        return Vec3f(1.f - (uv.x + uv.y) / uv.z, uv.y / uv.z, uv.x / uv.z);
+    }
+    return Vec3f{-1, 1, 1};
+}
+
+void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    bool lean = false;
+    if (std::abs((float)(y1 - y0) / (float)(x1 - x0)) > 1)
+    {
+        lean = true;
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+    }
+    if (x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    for (int x = x0; x < x1; x++)
+    {
+        float t = (float)(x - x0) / (float)(x1 - x0);
+        int y = y0 * (1 - t) + y1 * t;
+        image.set(lean ? y : x, lean ? x : y, color);
+    }
+}
+
+void draw_triangle(ScreenTriangle &t, TGAImage &image, TGAColor color)
+{
+    for (float y = t.ts[0].y; y < t.ts[2].y; y++)
+    {
+        float x0 = (y - t.ts[2].y) / (t.ts[0].y - t.ts[2].y) * (t.ts[0].x - t.ts[2].x) + t.ts[2].x;
+        float x1 = 0.f;
+        if (y < t.ts[1].y)
+        {
+            x1 = (y - t.ts[1].y) / (t.ts[0].y - t.ts[1].y) * (t.ts[0].x - t.ts[1].x) + t.ts[1].x;
+        }
+        else
+        {
+            x1 = (y - t.ts[1].y) / (t.ts[2].y - t.ts[1].y) * (t.ts[2].x - t.ts[1].x) + t.ts[1].x;
+        }
+        if (x0 > x1)
+        {
+            std::swap(x0, x1);
+        }
+        // 直到最后作图再转成int
+        for (float x = x0; x < x1; x++)
+        {
+            Vec3f uv = barycentric(t, Vec2f{x, y});
+            float z = Vec3f{t.ts[0].z, t.ts[1].z, t.ts[2].z} * uv;
+            if (z > zbuffer[(int)x][(int)y])
+            {
+                zbuffer[(int)x][(int)y] = z;
+                image.set(x, (int)y, color);
+            }
+        }
+    }
+}
