aee4bbf23b02d02ae0dd027c96c395db9cacb60e
李博轩 <211250189@smail.nju.edu.cn>
2025-04-02 17:04:51 +0800

    vector init...

--- main.cpp
+++ main.cpp
@@ -10,18 +10,18 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < model->nfaces(); i++)
     {
-        std::vector<Vec3f> verts{3};
-        std::vector<Vec3f> normals{3};
-        std::vector<float> intensities{3};
-        std::vector<Vec3f> screen_verts{3};
-        std::vector<ScreenTriangleVert> screen_tris{3};
+        std::vector<Vec3f> verts;
+        std::vector<Vec3f> normals;
+        std::vector<float> intensities;
+        std::vector<Vec3f> screen_verts;
+        std::vector<ScreenTriangleVert> screen_tris;
 
         for (int j = 0; j < 3; j++)
         {
-            verts[j] = model->vert(i, j);
-            normals[j] = model->normal(i, j);
+            verts.push_back(model->vert(i, j));
+            normals.push_back(model->normal(i, j));
 
-            intensities[j] = normals[j] * (verts[j] - light).normalize();
+            intensities.push_back(normals[j] * (verts[j] - light).normalize());
 
             Vec4f v4 = embed<4>(verts[j]);
             v4 = mvp * v4;
@@ -29,8 +29,8 @@ int main(int argc, char **argv)
             v4 = view_port * v4;
             // 转换成屏幕坐标时就窄化到int避免精度误差导致的像素空白
             int x = (int)v4[0], y = (int)v4[1];
-            screen_verts[j] = {(float)x, (float)y, v4[2]};
-            screen_tris[j] = ScreenTriangleVert{screen_verts[j], normals[j], intensities[j]};
+            screen_verts.push_back({(float)x, (float)y, v4[2]});
+            screen_tris.push_back(ScreenTriangleVert{screen_verts[j], normals[j], intensities[j]});
         }
 
         // for (int j = 0; j < 3; j++)
